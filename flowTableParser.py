20e5fd8c09221e02107c038c973a3e0c2e169401
Jun He <jhe@cs.wisc.edu>
2015-04-10 12:04:39 -0500

    be able to clean values

--- flowTableParser.py
+++ flowTableParser.py
@@ -1,6 +1,43 @@
 from pyparsing import *
 
+varname = Word(alphas+'_')
+value = Word(printables)
+assignment = varname.setResultsName('vname')\
+        + '=' + value.setResultsName('val')
+
+def parse_line(line):
+    row = {}
+    for tokens,start,end in assignment.scanString(line.replace(',', ' ')):
+        # print tokens.dump()
+        row[tokens.vname] = tokens.val
+    return row
+
 def text2table(linelist):
+    table = []
+    for line in linelist:
+        if not line.startswith(' cookie='):
+            continue
+        d = parse_line(line)
+        clean_value(d)
+        table.append(d)
+
+    return table
+
+def clean_value(d):
+    try:
+        d['duration'] = d['duration'].strip('s')
+    except KeyError:
+        pass
+
+line = "cookie=0x20000000000000, duration=147.156s, table=0, n_packets=146, n_bytes=14308, idle_timeout=5, idle_age=0, priority=1,ip,in_port=2,dl_src=02:a9:f3:97:09:a0,dl_dst=02:47:38:b8:cc:2a,nw_src=10.10.1.2,nw_dst=10.10.1.1 actions=output:1"
+
+# f = open('./zihao.log', 'r')
+f = open('./openflowdump-sample.log', 'r')
+print text2table(f.readlines())
+
+exit(0)
+
+def text2table_old(linelist):
     num = Word(nums)
     actionname = Word(printables)
     octnum = Word("0x"+hexnums)
